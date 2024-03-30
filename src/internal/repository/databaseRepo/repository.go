596563ace67a5236337d377b7d1b041b8c132b8a
Arafat <yeamin@sheba.xyz>
2024-03-30 12:15:49 +0600

    Health check api fixed

--- src/internal/repository/databaseRepo/repository.go
+++ src/internal/repository/databaseRepo/repository.go
@@ -3,14 +3,12 @@ package databaseRepo
 import (
 	"movie-journal-api/bootstrap"
 	"movie-journal-api/internal/driver/database"
-	"movie-journal-api/internal/logger"
 )
 
 var db *database.DB
 
 func (app *ApplicationRepo) getDB() *database.DB {
 	if db == nil {
-		logger.Info("db is nil", "")
 		db = app.InitializeDatabase()
 	}
 
@@ -22,7 +20,7 @@ type ApplicationRepo struct {
 }
 
 func NewApplicationRepo(app bootstrap.Application) *ApplicationRepo {
-	app.InitializeDatabase()
+	db = app.InitializeDatabase()
 	return &ApplicationRepo{
 		app,
 	}
